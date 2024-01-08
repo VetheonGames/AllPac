409345289e9c3224ac7a31e1b92c118f7f390d72
VetheonGames <ceo@pixelatedstudios.net>
2024-01-08 15:37:27 -0700

    Still trying to get Makepkg to stop yelling about root perms it doesn't really have

--- pkg/packagemanager/install.go
+++ pkg/packagemanager/install.go
@@ -167,7 +167,7 @@ func CloneAndInstallFromAUR(repoURL string, skipConfirmation bool) (string, erro
 
     // Build the package using makepkg as the non-root user
     cmdMakePkg := exec.Command("makepkg", "-si", "--noconfirm")
-    cmdMakePkg.Env = []string{"HOME=" + usr.HomeDir, "USER=" + usr.Username, "LOGNAME=" + usr.Username}
+    cmdMakePkg.Env = []string{"PATH=" + os.Getenv("PATH")}
     if output, err := cmdMakePkg.CombinedOutput(); err != nil {
         logger.Errorf("error building package with makepkg: %s, %v", output, err)
         return "", fmt.Errorf("error building package with makepkg: %s, %v", output, err)
